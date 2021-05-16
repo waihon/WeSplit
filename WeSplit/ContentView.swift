f50e6361ee4339e35f0d0151f4e1b8d38d82d1a8
Waihon Yew <yewwaihon@gmail.com>
2021-05-16 17:33:29 +0800

    Place the Form inside a NavigationView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Form {
-      Section {
-        Text("Hello, world!")
+    NavigationView {
+      Form {
+        Section {
+          Text("Hello, world!")
+        }
       }
     }
   }
