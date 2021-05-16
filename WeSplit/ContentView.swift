79cbe7d23c1d0b5182758827e064f195d92b9b72
Waihon Yew <yewwaihon@gmail.com>
2021-05-16 23:23:14 +0800

    Use shorthand syntax for the parameter name

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     Form {
-      ForEach(0 ..< 100) { number in
-        Text("Row \(number)")
+      ForEach(0 ..< 100) {
+        Text("Row \($0)")
       }
     }
   }
