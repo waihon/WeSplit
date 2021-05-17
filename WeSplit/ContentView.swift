1ba050fa727d185dcb56e91f8cfb6b643dffd642
Waihon Yew <yewwaihon@gmail.com>
2021-05-17 22:57:55 +0800

    Start with a clean slate for implementation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-  let students = ["Harry", "Hermione", "Ron", "Peter", "John"]
-  @State private var selectedStudent = 0
-  
   var body: some View {
-    VStack {
-      Picker("Select your student", selection: $selectedStudent) {
-        ForEach(0 ..< students.count) {
-          Text(self.students[$0])
-        }
-      }
-      Text("You choose: Student # \(students[selectedStudent])")
-    }
+    Text("Hello World")
   }
 }
 
