7bac37aa231a4827a6fd8ac373473a1674b340a1
Waihon Yew <yewwaihon@gmail.com>
2021-05-17 23:11:54 +0800

    Add the list of possible tip sizes

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
   @State private var checkAmount = ""
   @State private var numberOfPeople = 2
   @State private var tipPercentage = 2
+  let tipPercentages = [10, 15, 20, 25, 0]
   
   var body: some View {
     Text("Hello World")
